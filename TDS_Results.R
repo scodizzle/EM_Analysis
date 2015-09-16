a6bf1b9b428cc0998ffe6d8ab2293253ff2c345a
Scotty <scofro@gmail.com>
2015-09-16 01:47:39 -0700

    tried the new function, need to look at it in the AM

--- TDS_Results.R
+++ TDS_Results.R
@@ -6,29 +6,15 @@ library(splines)
 PO1.plot = EM.tds.graph(PO1, cline=0.2, sline=0.285, title="TDS PO1")
 
 
-PO1[PO1$DomRate >= 0.285 & PO1$taste == "Bitter",]
+PO1_A <- tds.subset(da = tds, factA = tds$Bottle, factB = tds$Attribute, samp = "PO1_A", n = 20)
+PO1_B <- tds.subset(da = tds, factA = tds$Bottle, factB = tds$Attribute, samp = "PO1_B", n = 20)
+PO1_C <- tds.subset(da = tds, factA = tds$Bottle, factB = tds$Attribute, samp = "PO1_C", n = 20)
 
 
-#subset for astringency
-PO1.ast <- PO1[PO1$DomRate >= 0.285 & PO1$taste == "Astringent",]
-#subtract the sig line
-PO1.ast$Dom0 <- PO1.ast$DomRate - 0.285
-#create spline function
-#other ways to do this
-dd <- splinefun(x=PO1.ast$time, y=PO1.ast$Dom0, method = "natural")
-#integrate stepwise
-intg <- lapply(1:(nrow(PO1.ast)-1), FUN = function(i) 
-  integrate(dd, lower = PO1.ast$time[i], upper = PO1.ast$time[i+1]))
-#add to the df
-PO1.ast$area <- c(0,unlist(lapply(1:43, FUN = function(i) intg[[i]]$value)))
-#culumlitive sum -- add to the df
-PO1.ast$areaCulm <- cumsum(PO1.ast$area)
-#length at max --- length at top
-lat <- nrow(PO1.ast[PO1.ast$DomRate == max(PO1.ast$DomRate),])
-sum(PO1.ast[PO1.ast$DomRate == max(PO1.ast$DomRate),])/lat
+aa <- tdsExt(PO1_A, "Astringent", 0.347)
+bb <- tdsExt(PO1_B, "Astringent", 0.347)
+cc <- tdsExt(PO1_C, "Astringent", 0.347)
 
-PO1.ast$spline <- unlist(lapply(PO1.ast$time, FUN = dd))
-(PO1.ast$Dom0 - PO1.ast$spline)^2
-
-apply(1:nrow(PO1.ast), FUN = function(i) integrate(dd, lower = PO1.ast$time[i], upper = PO1.ast$time[i+1]))
-
+aa$ExtParm
+bb$ExtParm
+cc$ExtParm
