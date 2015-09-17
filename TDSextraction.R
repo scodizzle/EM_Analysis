4215c431f0deaed5def77b21532ce6510050e3de
Scotty <scofro@gmail.com>
2015-09-17 11:21:39 -0700

    re did the funciton to have it integrate at each 1 second step.  then account for the sigLine

--- TDSextraction.R
+++ TDSextraction.R
@@ -5,25 +5,28 @@
 tdsExt.2 <- function(df, att, sigLine, ...){
   
   
-  df.e <- df[df$DomRate >= sigLine & df$taste == att,] #subset for astringency
+  #df.e <- df[df$DomRate >= sigLine & df$taste == att,] #subset for astringency
+  df.e <- df[df$taste == att,] #subset for astringency
   rownames(df.e) = 1:nrow(df.e)
-  df.e$Dom0 <- df.e$DomRate - sigLine #subtract the sig line
+  #df.e$Dom0 <- df.e$DomRate - sigLine #subtract the sig line
   
   #create spline function
   #other ways to do this, maybe alter the method....
-  dd <- splinefun(x=df.e$time, y=df.e$Dom0, method = "natural")
+  #dd <- splinefun(x=df.e$time, y=df.e$Dom0, method = "natural")
+  sfx <- splinefun(df.e$time, y=df.e$DomRate, method = "natural")
   
   #integrate stepwise
   idx <- nrow(df.e)-1
-  is <- lapply(1:idx, FUN = function(i) 
-    if(df.e$time[i+1] - df.e$time[i] == 1) {
-      integrate(dd, lower = df.e$time[i], upper = df.e$time[i+1])
-    } else { return(0) })
+  is <- lapply(0:90, FUN = function(i) integrate(sfx, lower = i, upper = i+1))
+  df.e$dxSpline <- unlist(lapply(1:91, FUN = function(i) if(is[[i]]$value < 0) {
+    return(0)} else {return(is[[i]]$value)})) #dx...  DROPS values less than 0!!
   
-  df.e$dxSpline <- c(0, unlist(lapply(1:idx, FUN = function(i) if(class(is[[i]]) == "integrate") 
-    {return(is[[i]]$value)} else {return(0)})))
-  df.e$SplineCulm <- cumsum(df.e$dxSpline) #culumlitive sum -- add to the df
+  df.e$dxCulu <- cumsum(dxSpline) # culumlitive sum of the dx
   
+  df.e$dxSigLine <- unlist(lapply(1:91, FUN = function(i) #account for the sigLine
+    if(df.e$DomRate[i] < sigLine) {return(0)} else {return(df.e$dxSpline[i] - sigLine)}))
+  
+
   #total parameters
   maxDomT <- max(df.e$DomRate) #max dom Rate 
   tmaxT <- nrow(df.e[df.e$DomRate == maxDomT,]) #length of time at max intensity 
@@ -41,7 +44,7 @@ tdsExt.2 <- function(df, att, sigLine, ...){
 
   
   ExtParm <- cbind(maxDomT,tmaxT,tAmaxT,areaT,ddT, maxDomPe,tmaxPe,tAmaxPe,areaPe,ddPe)
-  print(list(intg = df.e, ExtParm = ExtParm, spFUN = dd))
+  print(list(intg = df.e, ExtParm = ExtParm, spFUN = ss))
   
 }
 
