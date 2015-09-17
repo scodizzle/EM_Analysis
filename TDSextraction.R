e16e748c91065781b7c3bfff9b0037002e797743
Scotty <scofro@gmail.com>
2015-09-17 09:23:18 -0700

    last commit before a major revision!

--- TDSextraction.R
+++ TDSextraction.R
@@ -2,7 +2,7 @@
 ## takes data from the tds.subset function and pulls out 
 ##
 
-tdsExt <- function(df, att, sigLine, ...){
+tdsExt.2 <- function(df, att, sigLine, ...){
   
   
   df.e <- df[df$DomRate >= sigLine & df$taste == att,] #subset for astringency
@@ -15,29 +15,35 @@ tdsExt <- function(df, att, sigLine, ...){
   
   #integrate stepwise
   idx <- nrow(df.e)-1
-  is <- lapply(1:idx, FUN = function(i) integrate(dd, lower = df.e$time[i], upper = df.e$time[i+1]))
-  df.e$area <- c(0,unlist(lapply(1:idx, FUN = function(i) is[[i]]$value))) #add to the df
-  df.e$areaCulm <- cumsum(df.e$area) #culumlitive sum -- add to the df
+  is <- lapply(1:idx, FUN = function(i) 
+    if(df.e$time[i+1] - df.e$time[i] == 1) {
+      integrate(dd, lower = df.e$time[i], upper = df.e$time[i+1])
+    } else { return(0) })
+  
+  df.e$dxSpline <- c(0, unlist(lapply(1:idx, FUN = function(i) if(class(is[[i]]) == "integrate") 
+    {return(is[[i]]$value)} else {return(0)})))
+  df.e$SplineCulm <- cumsum(df.e$dxSpline) #culumlitive sum -- add to the df
   
   #total parameters
-  maxDomT <- max(df.e$DomRate) #max dom Rate
-  areaT <- df.e$areaCulm[nrow(df.e)] #total area under curve
+  maxDomT <- max(df.e$DomRate) #max dom Rate 
+  tmaxT <- nrow(df.e[df.e$DomRate == maxDomT,]) #length of time at max intensity 
+  tAmaxT <- sum(df.e[df.e$DomRate == maxDomT,]$time)/tmaxT #time of max dom rate
+  areaT <- df.e$SplineCulm[nrow(df.e)] #total area under curve
   ddT <- df.e$time[nrow(df.e)] - df.e$time[1] #time duration above the sig line
-  tmaxT <- nrow(df.e[df.e$DomRate == max(df.e$DomRate),]) #length of time at max intensity  
-  tAmaxT <- sum(df.e[df.e$DomRate == max(df.e$DomRate),]$time)/tmaxT #time of max dom rate
-  
+   
   #post Expecterate parameters
   pe <- df.e[df.e$time > 25,] #subset to only have post expecterate
   maxDomPe <- max(pe$DomRate) #max post expt DomRate
-  areaPe <- sum(pe$area) #sum up the area
+  tmaxPe <- nrow(pe[pe$DomRate == maxDomPe,]) #length of time at max intensity post exp  
+  tAmaxPe <- sum(pe[pe$DomRate == maxDomPe,]$time)/tmaxPe #time of max dom rate post exp
+  areaPe <- sum(pe$dxSpline) #sum up the area
   ddPe <- pe$time[nrow(pe)] - pe$time[1] #duration post Exp above sig line
-  tmaxPe <- nrow(pe[pe$DomRate == max(pe$DomRate),]) #length of time at max intensity post exp  
-  tAmaxPe <- sum(pe[pe$DomRate == max(pe$DomRate),]$time)/tmaxPe #time of max dom rate post exp
+
   
-  ExtParm <- cbind(maxDomT, maxDomPe, areaT, areaPe, ddT, ddPe, tmaxT, tmaxPe, tAmaxT, tAmaxPe)
-  print(list(intg = df.e, ExtParm = ExtParm))
+  ExtParm <- cbind(maxDomT,tmaxT,tAmaxT,areaT,ddT, maxDomPe,tmaxPe,tAmaxPe,areaPe,ddPe)
+  print(list(intg = df.e, ExtParm = ExtParm, spFUN = dd))
   
 }
 
-tdsExt(df=PO1, att="Astringent", sigLine = 0.285)
+test <- tdsExt.2(df=PO1, att="Astringent", sigLine = 0.285)
 
