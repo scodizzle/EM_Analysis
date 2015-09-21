d0690ab18ba3123bbee39b5bf9223e0e996155aa
Scotty <scofro@gmail.com>
2015-09-21 15:55:26 -0700

    set the function to use lm(y~ns(x, df)) to create a set of fitted spline values with df degrees of freedom.

--- TDSextForNorm.R
+++ TDSextForNorm.R
@@ -2,41 +2,38 @@
 ## takes data from the tds.subset function and pulls out 
 ## 
 
-tdsExtNORM<- function(df, att, sigLine, ...){
+tdsExtNORM<- function(dat, att, sigLine, df, ...){
   
+  dat <- dat[dat$sensation == att,] #subset for astringency
+  rownames(dat) = 1:nrow(dat)
   
-  #df <- df[df$domRate >= sigLine & df$taste == att,] #subset for astringency
-  df <- df[df$sensation == att,] #subset for astringency
-  rownames(df) = 1:nrow(df)
-  #df$Dom0 <- df$domRate - sigLine #subtract the sig line
-  
-  #create spline function <<-- this can be done abunch of different wasys
-  sfx <- splinefun(df$time, y=df$domRate, method = "natural")
+  dat.fit <- lm(dat$domRate ~ ns(dat$time, df), data=dat) #model the data!! use lm an  spline 
+  sfx <- splinefun(1:100, y=dat.fit$fitted.values , method = "natural")
   
   #integrate stepwise
-  is <- lapply(0:(nrow(df)-1), FUN = function(i) integrate(sfx, lower = i, upper = i+1))
-  df$dxSpline <- unlist(lapply(1:length(is), FUN = function(i) if(is[[i]]$value < 0) {
+  is <- lapply(0:(nrow(dat)-1), FUN = function(i) integrate(sfx, lower = i, upper = i+1))
+  dat$dxSpline <- unlist(lapply(1:length(is), FUN = function(i) if(is[[i]]$value < 0) {
     return(0)} else {return(is[[i]]$value)})) #dx...  DROPS values less than 0!!
   
-  df$dxCulu <- cumsum(df$dxSpline) # culumlitive sum of the dx
+  dat$dxCulu <- cumsum(dat$dxSpline) # culumlitive sum of the dx
   
   # account for the sigLine by subtracting from the dxSpline integration, not the area of 1 sec under the 
   # sigLine is the same valus as the sigLine  :-)
-  df$dxSigLine <- unlist(lapply(1:nrow(df), FUN = function(i) 
-    if(df$dxSpline[i] < sigLine) {return(0)} else {return(df$dxSpline[i] - sigLine)}))
+  dat$dxSigLine <- unlist(lapply(1:nrow(dat), FUN = function(i) 
+    if(dat$dxSpline[i] < sigLine) {return(0)} else {return(dat$dxSpline[i] - sigLine)}))
   
-  df$dxSigCulu <- cumsum(df$dxSigLine)
+  dat$dxSigCulu <- cumsum(dat$dxSigLine)
   
 
   #total parameters
-  area <- df$dxSigCulu[nrow(df)]    #Area above sig line
-  maxDom <- max(df$domRate) #max dom Rate 
-  tAmax <- nrow(df[df$domRate == maxDom,]) #length of time AT max intensity 
-  tmax <- sum(df[df$domRate == maxDom,]$time)/tAmax #time of max dom rate
-  dur <- colSums(df !=0)[7] #time duration above the sig line
+  area <- dat$dxSigCulu[nrow(dat)]    #Area above sig line
+  maxDom <- max(dat$domRate) #max dom Rate 
+  tAmax <- nrow(dat[dat$domRate == maxDom,]) #length of time AT max intensity 
+  tmax <- sum(dat[dat$domRate == maxDom,]$time)/tAmax #time of max dom rate
+  dur <- colSums(dat !=0)[7] #time duration above the sig line
    
   #post Expecterate parameters
-  #pe <- df[df$time > 25,] #subset to only have post expecterate
+  #pe <- dat[dat$time > 25,] #subset to only have post expecterate
   #maxDomPe <- max(pe$domRate) #max post expt domRate
   #tmaxPe <- nrow(pe[pe$domRate == maxDomPe,]) #length of time at max intensity post exp  
   #tAmaxPe <- sum(pe[pe$domRate == maxDomPe,]$time)/tmaxPe #time of max dom rate post exp
@@ -45,8 +42,9 @@ tdsExtNORM<- function(df, att, sigLine, ...){
 
   
   ExtParm <- data.frame(area,maxDom,tAmax,tmax,dur, row.names=1)
-  print(list(intg = df, ExtParm = ExtParm, spFUN = sfx))
+  print(list(intg = dat, ExtParm = ExtParm, spFUN = sfx))
   
 }
 
+tdsExtNORM(test, att = "Bitter", sigLine = 0.297, df = 18)
 
