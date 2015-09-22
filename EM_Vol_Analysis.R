5b59e4581460eef4b38f0ea07d6dbbc7452d44d9
Scotty <scofro@gmail.com>
2015-09-22 16:30:48 -0700

    scale and drop non sig, adjust the CVA to reflect the new plot looks

--- EM_Vol_Analysis.R
+++ EM_Vol_Analysis.R
@@ -13,34 +13,52 @@ library(reshape2)
 
 save(list = c("EMvol"), file = "EMvol.Rdata")
 
-# log of all and center scale
+# Boxplots of the various scaling methods
 boxplot(EMvol[,-c(1:9)])
 #boxplot(scale(EMvol[,-c(1:9)]))
 #boxplot(log(EMvol[,-c(1:9)]))
 #boxplot(scale(log10(EMvol[,-c(1:9)])))
 
-logScaleVol <- cbind(EMvol[,1:9], (EMvol[,-c(1:9)]))
-#logScaleVol <- cbind(EMvol[,1:9], scale(EMvol[,-c(1:9)]))
-#logScaleVol <- cbind(EMvol[,1:9], log(EMvol[,-c(1:9)]))
-#logScaleVol <- cbind(EMvol[,1:9], scale(log(EMvol[,-c(1:9)])))
+#Scale the data no log tho...
+ScaleVol <- cbind(EMvol[,1:9], scale(EMvol[,-c(1:9)], center = TRUE)) #mean centerscale the vol data
 
-volMan <- manova(as.matrix(logScaleVol[,-c(1:9)]) ~ wine, data = logScaleVol)
+#####  ANOVA of the scaled DATA  #####
+vol.lm <- lm(as.matrix(ScaleVol[,-c(1:9)]) ~ wine + wine/fermRep, data = ScaleVol)
+summary.aov(vol.lm)
+# All significant but ethyl.decanoate  vitispirane.I.and.II
+
+# drop ethyl.decanoate & vitispirane.I.and.II
+volSig <- ScaleVol[,-which(names(ScaleVol) %in% c("ethyl.decanoate", "vitispirane.I.and.II"))]
+
+# CVA of the significant measurments
+volMan <- manova(as.matrix(volSig[,-c(1:9)]) ~ wine, data = volSig)
 summary(volMan, test = "Wilks")
 volCVA <- candisc(volMan, scale=FALSE)
 plot(volCVA)
 
 # CVA Plot
-volPlot <- ggplot(volCVA$means, aes(x=Can1, y=Can2, label=row.names(volCVA$means))) +
+ggplot(volCVA$means, aes(x=Can1, y=Can2, label=row.names(volCVA$means))) +
   geom_text(family = "Times New Roman", fontface="bold", size=7) +
-  geom_segment(data=as.data.frame(volCVA$coeffs.std), aes(x=0, y=0, xend=Can1*5, yend=Can2*5, label=row.names(volCVA$coeffs.std)), 
+  geom_segment(data=as.data.frame(volCVA$coeffs.std), aes(x=0, y=0, xend=Can1*3.5, yend=Can2*3.5, label=row.names(volCVA$coeffs.std)), 
                arrow=arrow(length=unit(0.3,"cm")), color="grey", size=1) +
   geom_text(data=as.data.frame(volCVA$coeffs.std), 
-            aes(x=Can1*5, y=Can2*5, label=row.names(volCVA$coeffs.std)), family = "Times New Roman", fontface = "italic") +
-  scale_x_continuous(paste("Can 1 ", "(", round(volCVA$pct[1],1), "%", ")", sep=""), limits = c(-27,27)) +
-  scale_y_continuous(paste("Can 2 ", "(", round(volCVA$pct[2],1), "%", ")", sep=""), limits = c(-27,27))+
+            aes(x=Can1*3.5, y=Can2*3.5, label=row.names(volCVA$coeffs.std)), family = "Times New Roman", fontface = "italic") +
+  scale_x_continuous(paste("Can 1 ", "(", round(volCVA$pct[1],1), "%", ")", sep=""), limits = c(-27,20)) +
+  scale_y_continuous(paste("Can 2 ", "(", round(volCVA$pct[2],1), "%", ")", sep="")) +
   theme(axis.text = element_text(size=16, color="black", family = "Times New Roman"),
         axis.title = element_text(size=16, color="black", family = "Times New Roman", face = "bold"),
         panel.background = element_rect(fill = "transparent"),
         panel.border = element_rect(linetype = "solid", color = "black", fill=NA),
         panel.grid.major = element_line(color="transparent"))
+
+
+
+
+
+
+
+
+
+
+
 
