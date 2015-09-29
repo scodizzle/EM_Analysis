0df4ad3fcac4237986a352324c25cc7cf2612535
Scotty <scofro@gmail.com>
2015-09-28 17:23:15 -0700

    changed the df

--- TDS_Results.R
+++ TDS_Results.R
@@ -19,7 +19,7 @@ load("tds.Rdata")
 
 # extract curve parameters using the tdsExtNORM f(X) 
 ## Bitter
-emExt.Bitter<- lapply(norm.ready, tdsExtNORM, att="Bitter", sigLine=0.297, df=18)
+emExt.Bitter<- lapply(norm.ready, tdsExtNORM, att="Bitter", sigLine=0.297, df=5)
 Bitter <- do.call(rbind.data.frame, lapply(1:27, function(i) emExt.Bitter[[i]]$ExtParm))
 Bitter$wine <- names(emExt.Bitter)
 row.names(Bitter) = 1:27
@@ -45,9 +45,8 @@ cbind.data.frame(
   Bitter.maxDom$statistics[4],
   Bitter.dur$statistics[4])
 
-
 ## HOT
-emExt.Hot<- lapply(norm.ready, tdsExtNORM, att="Hot", sigLine=0.297, df=18)
+emExt.Hot<- lapply(norm.ready, tdsExtNORM, att="Hot", sigLine=0.297, df=5)
 Hot<- do.call(rbind.data.frame, lapply(1:27, function(i) emExt.Hot[[i]]$ExtParm))
 Hot$wine <- names(emExt.Hot)
 row.names(Hot) = 1:27
@@ -56,10 +55,13 @@ Hot$fermRep <- unlist(lapply(1:27, FUN = function(i) strsplit(Hot$wine, "_")[[i]
 Hot$sensation <- rep("Hot", nrow(Hot))
 Hot.lm <- lm(as.matrix(Hot[,c(1:5)]) ~ trt + fermRep, data = Hot)
 summary.aov(Hot.lm) 
-# No Sig for Hot
+# Hot duration
+Hot.dur <- LSD.test(lm(dur~trt+fermRep, data=Hot), trt="trt", group=TRUE)
+Hot.dur
+Hot.dur$statistics[4]
 
 ## Sweet
-emExt.Sweet<- lapply(norm.ready, tdsExtNORM, att="Sweet", sigLine=0.297, df=18)
+emExt.Sweet<- lapply(norm.ready, tdsExtNORM, att="Sweet", sigLine=0.297, df=5)
 Sweet <- do.call(rbind.data.frame, lapply(1:27, function(i) emExt.Sweet[[i]]$ExtParm))
 Sweet$wine <- names(emExt.Sweet)
 row.names(Sweet) = 1:27
@@ -71,7 +73,7 @@ summary.aov(Sweet.lm)
 # No Sig for Sweet
 
 ## Sour
-emExt.Sour<- lapply(norm.ready, tdsExtNORM, att="Sour", sigLine=0.297, df=18)
+emExt.Sour<- lapply(norm.ready, tdsExtNORM, att="Sour", sigLine=0.297, df=5)
 Sour <- do.call(rbind.data.frame, lapply(1:27, function(i) emExt.Sour[[i]]$ExtParm))
 Sour$wine <- names(emExt.Sour)
 row.names(Sour) = 1:27
@@ -83,7 +85,7 @@ summary.aov(Sour.lm)
 # No significant difference for Sour
 
 ## Astringent
-emExt.Ast<- lapply(norm.ready, tdsExtNORM, att="Astringent", sigLine=0.297, df=18)
+emExt.Ast<- lapply(norm.ready, tdsExtNORM, att="Astringent", sigLine=0.297, df=5)
 Ast <- do.call(rbind.data.frame, lapply(1:27, function(i) emExt.Ast[[i]]$ExtParm))
 Ast$wine <- names(emExt.Ast)
 row.names(Ast) = 1:27
@@ -101,96 +103,89 @@ tdsAll$trt <- factor(tdsAll$trt)
 tdsAll$sensation <- factor(tdsAll$sensation)
 tdsAll$fermRep <- factor(tdsAll$fermRep)
 
-## this approach of nesting the sensation w/in the fermRep...  
+## this "tips" the tdsAll over to reorganize it, as tdsAll is a df of the extracted paramters from the
+## 
+    tdsAll.t <- cbind(tdsAll[tdsAll$sensation == "Bitter",], tdsAll[tdsAll$sensation == "Astringent",], 
+                  tdsAll[tdsAll$sensation == "Sweet",], tdsAll[tdsAll$sensation == "Sour",], 
+                  tdsAll[tdsAll$sensation == "Hot",])
+    colnames(tdsAll.t) <- c(
+      "B.area","B.maxDom","B.tAmax","B.tmax","B.dur","wine","trt","fermRep", "sensation",
+      "A.area","A.maxDom","A.tAmax","A.tmax","A.dur","wine","trt","fermRep", "sensation",
+      "Sw.area","Sw.maxDom","Sw.tAmax","Sw.tmax","Sw.dur","wine","trt","fermRep", "sensation",
+      "So.area","So.maxDom","So.tAmax","So.tmax","So.dur","wine","trt","fermRep", "sensation",
+      "H.area","H.maxDom","H.tAmax","H.tmax","H.dur","wine","trt","fermRep", "sensation")
+    tdsAll.t <- tdsAll.t[,-c(6:9,15:18,24:27,33:36, 45)]
+    tdsAll.t <- cbind(tdsAll.t[,c(26:28)], tdsAll.t[,c(1:25)])
+    
+    #Note....  this gives the same results as the indivdual lm analysis above
+    summary.aov(lm(as.matrix(tdsAll.t[,-c(1:3)]) ~ trt + fermRep, data = tdsAll.t))  
+
+## this approach of nesting the sensation w/in the fermRep... 
+manova(as.matrix(tdsAll.t[,-c(1:3)]) ~ trt + fermRep, data = tdsAll.t)#FAILS
 summary.aov(lm(as.matrix(tdsAll[,c(1:5)]) ~ trt + fermRep + fermRep/sensation, data=tdsAll))
-summary.aov(lm(as.matrix(tdsAll.t[,-c(1:3)]) ~ trt + fermRep, data=tdsAll.t))
 
-#####
-summary(manova(as.matrix(tdsAll.t[,-c(1:3)]) ~ trt + fermRep, data = tdsAll.t), test="Wilks")
 
-# manova for all tds extracted paramters
-tds.m <- manova(as.matrix(tdsAll[,c(1:5)]) ~ trt + sensation, data = tdsAll)
-summary(tds.m, test="Wilks") # not significant
-
+######################################################################################################
+###### MANOVA of the TDS dataset
+## start with the extracted dataset..  the tdsAll.t 
+head(tdsAll.t)
+# use the 4 extracted significant parameters for CVA
+tdsSig <- manova(as.matrix(tdsAll.t[,c("B.area","B.maxDom","B.dur","H.dur")]) ~ trt, data=tdsAll.t)
+    summary(tdsSig, test="Wilks") # SIGNIFICANT
+tdsSigCVA <- candisc(tdsSig)
+    plot(tdsSigCVA)
+# using the tdsAll df for the manova
+# all sensations
+tds.m <- manova(as.matrix(tdsAll[,c(1:5)]) ~ trt +fermRep + fermRep/sensation, data = tdsAll)
+  summary(tds.m, test="Wilks") # not significant
 # just bitter and Astringent
-tds.BA <- manova(as.matrix(tdsAll[tdsAll$sensation %in% c("Bitter","Astringent"),][,c(1:5)]) ~ trt + fermRep + fermRep/sensation, 
-                 data = tdsAll[tdsAll$sensation %in% c("Bitter","Astringent"),])
-summary(tds.BA, test="Wilks") # significant
-# bitter and astringent together are sig on Wilks, 
-# make a CVA plot
-tdsCVA <- candisc(tds.BA)
-plot(tdsCVA)
+tds.BA <- manova(as.matrix(
+  tdsAll[tdsAll$sensation %in% c("Bitter","Astringent"),][,c(1:5)]) ~ trt + fermRep + fermRep/sensation, 
+  data = tdsAll[tdsAll$sensation %in% c("Bitter","Astringent"),])
+    summary(tds.BA, test="Wilks") # significant
+tdsBA.CVA <- candisc(tds.BA)
+  plot(tdsBA.CVA)
 
-# just the significant bitter terms
-tds.b <- manova(as.matrix(tdsAll[tdsAll$sensation == "Bitter",][,c("area","maxDom","dur")]) ~ trt, 
-                data = tdsAll[tdsAll$sensation == "Bitter",]) 
-summary(tds.b, test = "Wilks") # significant for only Wilks 
-tds.b.CVA <- candisc(tds.b)
-plot(tds.b.CVA)
-# CVA plot of the bitter significant terms
-ggplot(tds.b.CVA$means, aes(x=Can1, y=Can2, label=row.names(tds.b.CVA$means))) +
+##### CVA plots
+## tdsSig
+ggplot(tdsSigCVA$means, aes(x=Can1, y=Can2, label=row.names(tdsSigCVA$means))) +
   geom_text(family = "Times New Roman", fontface="bold", size=7) +
-  geom_segment(data=as.data.frame(tds.b.CVA$structure), aes(x=0, y=0, xend=Can1*2, yend=Can2*2, label=row.names(tds.b.CVA$structure)), 
+  geom_segment(data=as.data.frame(tdsSigCVA$structure), aes(x=0, y=0, xend=Can1, yend=Can2, label=row.names(tdsSigCVA$structure)), 
                arrow=arrow(length=unit(0.3,"cm")), color="grey", size=1) +
-  geom_text(data=as.data.frame(tds.b.CVA$structure), 
-            aes(x=Can1*2, y=Can2*2, label=row.names(tds.b.CVA$structure)), family = "Times New Roman", fontface = "italic") +
-  scale_x_continuous(paste("Can 1 ", "(", round(tds.b.CVA$pct[1],1), "%", ")", sep="")) +
-  scale_y_continuous(paste("Can 2 ", "(", round(tds.b.CVA$pct[2],1), "%", ")", sep="")) +
+  geom_text(data=as.data.frame(tdsSigCVA$structure), 
+            aes(x=Can1, y=Can2, label=row.names(tdsSigCVA$structure)), family = "Times New Roman", fontface = "italic") +
+  scale_x_continuous(paste("Can 1 ", "(", round(tdsSigCVA$pct[1],1), "%", ")", sep="")) +
+  scale_y_continuous(paste("Can 2 ", "(", round(tdsSigCVA$pct[2],1), "%", ")", sep="")) +
+  theme(axis.text = element_text(size=16, color="black", family = "Times New Roman"),
+        axis.title = element_text(size=16, color="black", family = "Times New Roman", face = "bold"),
+        panel.background = element_rect(fill = "transparent"),
+        panel.border = element_rect(linetype = "solid", color = "black", fill=NA),
+        panel.grid.major = element_line(color="transparent"))
+## tdsBA.CVA
+ggplot(tdsBA.CVA$means, aes(x=Can1, y=Can2, label=row.names(tdsBA.CVA$means))) +
+  geom_text(family = "Times New Roman", fontface="bold", size=7) +
+  geom_segment(data=as.data.frame(tdsBA.CVA$structure), aes(x=0, y=0, xend=Can1, yend=Can2, label=row.names(tdsBA.CVA$structure)), 
+               arrow=arrow(length=unit(0.3,"cm")), color="grey", size=1) +
+  geom_text(data=as.data.frame(tdsBA.CVA$structure), 
+            aes(x=Can1, y=Can2, label=row.names(tdsBA.CVA$structure)), family = "Times New Roman", fontface = "italic") +
+  scale_x_continuous(paste("Can 1 ", "(", round(tdsBA.CVA$pct[1],1), "%", ")", sep="")) +
+  scale_y_continuous(paste("Can 2 ", "(", round(tdsBA.CVA$pct[2],1), "%", ")", sep="")) +
   theme(axis.text = element_text(size=16, color="black", family = "Times New Roman"),
         axis.title = element_text(size=16, color="black", family = "Times New Roman", face = "bold"),
         panel.background = element_rect(fill = "transparent"),
         panel.border = element_rect(linetype = "solid", color = "black", fill=NA),
         panel.grid.major = element_line(color="transparent"))
 
-
+######################################################################################################
+######################################################################################################
 ##### 
 ## some aditional plots
-## TDS but facet the plot by wine
-## facet grid
-
-ggplot(tdsPD[tdsPD$sensation %in% c("Bitter", "Astringent", "Sweet", "Sour", "Hot"),], aes(x=time, y=domRate)) +
-         geom_smooth(method="lm", formula= y~ns(x,18), se=FALSE, guide=TRUE, color="black") +
-         geom_polygon(data = data.frame(x=c(0,100,100,0), y=c(0,0,0.297,0.297)), aes(x=x, y=y), alpha=0.1) +
-         facet_grid(wine~sensation) +
-         scale_x_continuous(breaks=c(50), name = "Normalized Time") +
-         scale_y_continuous(breaks=c(0.50), name = "Dominance Rate (%)") +
-         theme(axis.text = element_text(size=14), 
-               axis.title=element_text(size=16),
-               panel.background = element_rect(fill = "transparent"),
-               #panel.grid.major = element_line(colour = "black", size=0.75),
-               #panel.grid.minor = element_line(colour = "black", size=0.75),
-               panel.border = element_rect(linetype = "solid", colour = "black", fill=NA),
-               strip.text.y = element_text(colour = "black", size = 14),
-               legend.text = element_text(size = 13, color = "black"),
-               legend.title = element_text(size=13),
-               legend.key = element_rect(fill = "transparent"))
-  
-
-
-
-
 ## create data for plotting...
-######################################################################################################
 ######################################################################################################
 PD <- preplotTDS(rbind.data.frame(normFrame.wine$PD_A, normFrame.wine$PD_B, normFrame.wine$PD_C))
 PD$wine <- rep("PD", 600)
 PD$bottle <- rep()
 row.names(PD) = 1:600
-
-PDA <- preplotTDS(normFrame.wine$PD_A)
-PDA$wine <- rep("PDA", 600)
-row.names(PDA) = 1:600
-PDB <- preplotTDS(normFrame.wine$PD_B)
-PDB$wine <- rep("PDB", 600)
-row.names(PDB) = 1:600
-PDC <- preplotTDS(normFrame.wine$PD_C)
-PDC$wine <- rep("PDC", 600)
-row.names(PDC) = 1:600
-
-tdsPDA <- rbind.data.frame(PDA, PDB, PDC)
-
-
-
 
 POC <- preplotTDS(rbind.data.frame(normFrame.wine$POC_A, normFrame.wine$POC_B, normFrame.wine$POC_C))
 POC$wine <- rep("POC", 600) 
@@ -225,6 +220,27 @@ S8$wine <- rep("S8", 600)
 row.names(S8) = 1:600
 tdsNORMall <- rbind.data.frame(PD, POC, SUB, PO1, PO2, PO4, PO6, PO8, S8)
 rm(PD, POC, SUB, PO1, PO2, PO4, PO6, PO8, S8)
+
+## TDS but facet the plot by wine
+## facet grid
+ggplot(tdsNORMall[tdsNORMall$sensation %in% c("Bitter", "Astringent", "Sweet", "Sour", "Hot"),], aes(x=time, y=domRate)) +
+         geom_smooth(method="lm", formula= y~ns(x,5), se=FALSE, guide=TRUE, color="black") +
+         geom_polygon(data = data.frame(x=c(0,100,100,0), y=c(0,0,0.297,0.297)), aes(x=x, y=y), alpha=0.1) +
+         facet_grid(wine~sensation) +
+         scale_x_continuous(breaks=c(50), name = "Normalized Time") +
+         scale_y_continuous(breaks=c(0.50), name = "Dominance Rate (%)") +
+         theme(axis.text = element_text(size=14), 
+               axis.title=element_text(size=16),
+               panel.background = element_rect(fill = "transparent"),
+               #panel.grid.major = element_line(colour = "black", size=0.75),
+               #panel.grid.minor = element_line(colour = "black", size=0.75),
+               panel.border = element_rect(linetype = "solid", colour = "black", fill=NA),
+               strip.text.y = element_text(colour = "black", size = 14),
+               legend.text = element_text(size = 13, color = "black"),
+               legend.title = element_text(size=13),
+               legend.key = element_rect(fill = "transparent"))
+  
+
 
 
 
